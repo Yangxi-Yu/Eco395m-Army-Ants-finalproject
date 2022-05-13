fc627fac60459c07f01c5e8ed7d5ef935d9756c2
Chen Tang <tangchen@wireless-10-147-3-218.public.utexas.edu>
2022-05-12 22:10:58 -0500

    update

--- code/Dashboard-sql/view_hybrid.sql
+++ code/Dashboard-sql/view_hybrid.sql
@@ -1,6 +1,6 @@
 select *,
 case when "location" like '%Remote%' then 'Remote'
 when "location" like '%Hybrid%' then 'Hybrid'
-else 'In person'
+else 'in person'
 end as  job_type
 from job_basic_information_all 
