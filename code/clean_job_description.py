f29c491e66baaf2265b826df146bd5f79cc58dfa
xiaohanwu98 <jessicawu98@utexas.edu>
2022-05-13 02:47:13 +0000

    Added the comments

--- code/clean_job_description.py
+++ code/clean_job_description.py
@@ -1,3 +1,4 @@
+# import the packages and libraries
 from database import engine
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -9,7 +10,7 @@ from sqlalchemy.types import String, Numeric
 import re
 
 
-
+# perform the data cleaning, and generated a cleaned HTML text column to the data frame
 def add_cleaned_html_text_column(job_description_html_df):
     len_df = len(job_description_html_df)
     job_description_html_df['clean_description'] = ''
@@ -18,22 +19,23 @@ def add_cleaned_html_text_column(job_description_html_df):
         script = html.find_all(id = 'jobDescriptionText')
         if script != []:
             extract_text = script[0].text
-            text_remove_punctuation = re.sub('[^A-Za-z\s]', '', extract_text.lower())
-            text_with_only_spaces = re.sub("\s+", " ", text_remove_punctuation)
-            cleaned_text = re.sub('\n', ' ', text_with_only_spaces)
+            text_remove_punctuation = re.sub('[^A-Za-z\s]', '', extract_text.lower())   # remove the punctuations
+            text_with_only_spaces = re.sub("\s+", " ", text_remove_punctuation)   # remove the unmeaningful words
+            cleaned_text = re.sub('\n', ' ', text_with_only_spaces)  
             job_description_html_df['clean_description'][i] = cleaned_text
     return job_description_html_df
 
-
+# perform the data cleaning, remove soft words, and keep the meaningful verbals, nouns, and adjectives
 def add_v_n_j_only_description(job_description_html_df, raw_column_name, new_column):
     job_description_html_df[new_column] = ''
     len_train = len(job_description_html_df)
     for row in range(0, len_train):
         clean_description = job_description_html_df[raw_column_name][row]
-        split_by_word = word_tokenize(clean_description)
+        split_by_word = word_tokenize(clean_description)   # split into word by word
 
         n_v_j_txt = ""
-        find_part_of_speech = nltk.pos_tag(split_by_word)
+        find_part_of_speech = nltk.pos_tag(split_by_word)       
+        # remove unmeaningful soft words
         for val in find_part_of_speech:
             if re.search(r'^V',val[1]) or re.search(r'^N',val[1]) or re.search(r'^J',val[1]):
                 n_v_j_txt += val[0] 
